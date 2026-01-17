0a5d8da1d3e9f90638e130c731e69806499f3b89
Isaque_Alvess <ysak.alves@outlook.com>
2026-01-17 20:03:10 -0300

    chore: separate backend and frontend into dedicated folders

---
+++ backend/src/main/java/com/task_estimator/TaskEstimatorApplication.java
@@ -0,0 +1,13 @@
+package com.task_estimator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskEstimatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskEstimatorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/task_estimator/TaskEstimatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task_estimator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskEstimatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
